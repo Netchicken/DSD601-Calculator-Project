0281bb287396d283605cef259714a46e43073a98
Gary Dix <intelproof@gmail.com>
2025-03-20 09:29:17 +1300

    Original Calculator reverted back

--- Operations/Calculations.cs
+++ Operations/Calculations.cs
@@ -1,61 +1,59 @@
-﻿using DSD601CalculatorASP.DTO;
-
-namespace DSD601CalculatorASP.Operations
+﻿namespace DSD601CalculatorASP.Operations
 {
-    public class Calculations
-    {
-        public void Calculation(Calculator calculator)
-        {
-            if (calculator.OperationType == EnumOperationType.Addition)
-                calculator.Result = calculator.NumberA + calculator.NumberB;
-            if (calculator.OperationType == EnumOperationType.Division)
-                calculator.Result = calculator.NumberA / calculator.NumberB;
-            if (calculator.OperationType == EnumOperationType.Subtraction)
-                calculator.Result = calculator.NumberA - calculator.NumberB;
-            if (calculator.OperationType == EnumOperationType.Multiplication)
-                calculator.Result = calculator.NumberA * calculator.NumberB;
-        }
+    //public class Calculations
+    //{
+    //    public void Calculation(Calculator calculator)
+    //    {
+    //        if (calculator.OperationType == EnumOperationType.Addition)
+    //            calculator.Result = calculator.NumberA + calculator.NumberB;
+    //        if (calculator.OperationType == EnumOperationType.Division)
+    //            calculator.Result = calculator.NumberA / calculator.NumberB;
+    //        if (calculator.OperationType == EnumOperationType.Subtraction)
+    //            calculator.Result = calculator.NumberA - calculator.NumberB;
+    //        if (calculator.OperationType == EnumOperationType.Multiplication)
+    //            calculator.Result = calculator.NumberA * calculator.NumberB;
+    //    }
 
-        //lets refactor the method to a Pure Method - when we get to Unit Testing then it will be ready to be tested.
-        public Calculator CalculationPureMethod(Calculator calculator)
-        {
-            if (calculator.OperationType == EnumOperationType.Addition)
-                calculator.Result = calculator.NumberA + calculator.NumberB;
-            if (calculator.OperationType == EnumOperationType.Division)
-                calculator.Result = calculator.NumberA / calculator.NumberB;
-            if (calculator.OperationType == EnumOperationType.Subtraction)
-                calculator.Result = calculator.NumberA - calculator.NumberB;
-            if (calculator.OperationType == EnumOperationType.Multiplication)
-                calculator.Result = calculator.NumberA * calculator.NumberB;
-            return calculator;
-        }
+    //    //lets refactor the method to a Pure Method - when we get to Unit Testing then it will be ready to be tested.
+    //    public Calculator CalculationPureMethod(Calculator calculator)
+    //    {
+    //        if (calculator.OperationType == EnumOperationType.Addition)
+    //            calculator.Result = calculator.NumberA + calculator.NumberB;
+    //        if (calculator.OperationType == EnumOperationType.Division)
+    //            calculator.Result = calculator.NumberA / calculator.NumberB;
+    //        if (calculator.OperationType == EnumOperationType.Subtraction)
+    //            calculator.Result = calculator.NumberA - calculator.NumberB;
+    //        if (calculator.OperationType == EnumOperationType.Multiplication)
+    //            calculator.Result = calculator.NumberA * calculator.NumberB;
+    //        return calculator;
+    //    }
 
-        //lets use a switch statemnt with the latest tools -- WHEN  https://ochzhen.com/blog/switch-case-when-in-csharp
-        public double CalculationPureMethodSwitch(Calculator calculator)
-        {
+    //    //lets use a switch statemnt with the latest tools -- WHEN  https://ochzhen.com/blog/switch-case-when-in-csharp
+    //    public double CalculationPureMethodSwitch(Calculator calculator)
+    //    {
 
-            switch (calculator)
-            {
-                case Calculator when calculator.OperationType == EnumOperationType.Addition:
-                    calculator.Result = calculator.NumberA + calculator.NumberB;
-                    break;
+    //        switch (calculator)
+    //        {
+    //            case Calculator when calculator.OperationType == EnumOperationType.Addition:
+    //                calculator.Result = calculator.NumberA + calculator.NumberB;
+    //                break;
 
-                case Calculator when calculator.OperationType == EnumOperationType.Division:
-                    calculator.Result = calculator.NumberA / calculator.NumberB;
-                    break;
+    //            case Calculator when calculator.OperationType == EnumOperationType.Division:
+    //                calculator.Result = calculator.NumberA / calculator.NumberB;
+    //                break;
 
-                case Calculator when calculator.OperationType == EnumOperationType.Subtraction:
-                    calculator.Result = calculator.NumberA - calculator.NumberB;
-                    break;
+    //            case Calculator when calculator.OperationType == EnumOperationType.Subtraction:
+    //                calculator.Result = calculator.NumberA - calculator.NumberB;
+    //                break;
 
-                case Calculator when calculator.OperationType == EnumOperationType.Multiplication:
-                    calculator.Result = calculator.NumberA * calculator.NumberB;
-                    break;
+    //            case Calculator when calculator.OperationType == EnumOperationType.Multiplication:
+    //                calculator.Result = calculator.NumberA * calculator.NumberB;
+    //                break;
 
 
-            }
+    //        }
 
-            return calculator.Result;
-        }
-    }
+    //        return calculator.Result;
+    //    }
+    //}
 }
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,23 +1,50 @@
-﻿using DSD601CalculatorASP.DTO;
-using DSD601CalculatorASP.Operations;
-
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using System.ComponentModel.DataAnnotations;
 
 namespace DSD601CalculatorASP.Pages
 {
     [BindProperties] //this connects the properties in the back end to the Model in the front it Binds the Properties
     public class IndexModel : PageModel
     {
-        public Calculator calculator { get; set; }
-        public Calculations calculations { get; set; }
+        //  public Calculator calculator { get; set; }
+        //  public Calculations calculations { get; set; }
+
+        [Display(Name = "First Number")] //these are attributes that you can use to change and control what is seen and done int he front end
+        public double NumberA { get; set; }
+
+        [Display(Name = "Second Number")]
+        public double NumberB { get; set; }
+
+        [Display(Name = "Result")] //holds the result of the operation
+        public double Result { get; set; }
+
+        [Display(Name = "Operation")]
+        public EnumOperationType OperationType { get; set; } //this is the Enum with is a handy way to make a list easily.
+
+
+
 
         //this runs when you submit the form
         public void OnPostAsync()
         {
             if (ModelState.IsValid)
             {
-                calculations.Calculation(calculator);
+
+                if (OperationType == EnumOperationType.Addition)
+                    Result = NumberA + NumberB;
+                if (OperationType == EnumOperationType.Division)
+                    Result = NumberA / NumberB;
+                if (OperationType == EnumOperationType.Subtraction)
+                    Result = NumberA - NumberB;
+                if (OperationType == EnumOperationType.Multiplication)
+                    Result = NumberA * NumberB;
+
+
+
+
+                //   calculations.Calculation(calculator);
             }
         }
     }
@@ -54,33 +81,14 @@ namespace DSD601CalculatorASP.Pages
 
 
 
-//if (calculator.OperationType == EnumOperationType.Addition)
-//    calculator.Result = calculator.NumberA + calculator.NumberB;
-//if (calculator.OperationType == EnumOperationType.Division)
-//    calculator.Result = calculator.NumberA / calculator.NumberB;
-//if (calculator.OperationType == EnumOperationType.Subtraction)
-//    calculator.Result = calculator.NumberA - calculator.NumberB;
-//if (calculator.OperationType == EnumOperationType.Multiplication)
-//    calculator.Result = calculator.NumberA * calculator.NumberB;
-
-//[Display(Name = "First Number")] //these are attributes that you can use to change and control what is seen and done int he front end
-//public double NumberA { get; set; }
-
-//[Display(Name = "Second Number")]
-//public double NumberB { get; set; }
-
-//[Display(Name = "Result")] //holds the result of the operation
-//public double Result { get; set; }
-
-//[Display(Name = "Operation")]
-//public EnumOperationType OperationType { get; set; } //this is the Enum with is a handy way to make a list easily.
 
 
 
-//public enum EnumOperationType
-//{
-//    Addition,
-//    Multiplication,
-//    Division,
-//    Subtraction
-//}
+
+public enum EnumOperationType
+{
+    Addition,
+    Multiplication,
+    Division,
+    Subtraction
+}
